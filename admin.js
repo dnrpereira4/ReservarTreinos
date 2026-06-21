f66d90836f189424933eb43d8d987549a704458e
dnrpereira4 <dnrpereira4@gmail.com>
2026-06-21 20:40:57 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "A_TUA_URL";
-const SUPABASE_KEY = "A_TUA_KEY";
+const SUPABASE_URL = "https://kpcmfblvdkpojrwjqorz.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwY21mYmx2ZGtwb2pyd2pxb3J6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NDE1OTQsImV4cCI6MjA5NzUxNzU5NH0.AgjDrNEYGyega75VVx5Mib9V4b2mH0ZkzcJaaunwng4";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
